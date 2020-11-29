27d35d6ef48e80858ae0b2a7efae33ad5cc6d68d
mjperezg2010 <mjperezg2010@unitec.edu>
2020-11-29 12:08:30 -0600

    Enabled Save button and creating crypting function

--- Front_end_angular/app-encryptor-decryptor/src/app/principal/principal.component.ts
+++ Front_end_angular/app-encryptor-decryptor/src/app/principal/principal.component.ts
@@ -13,7 +13,8 @@ export class PrincipalComponent implements OnInit {
   actualList:String[]=[]
   actualUser:String
   selected:String
-  originalText:String=""
+  originalText:String
+  encrypText:String
 
   constructor(public appService:AppserviceService,public auth:AuthService) { }
 
@@ -29,6 +30,7 @@ export class PrincipalComponent implements OnInit {
 
   clickFavorite(text:String){
     this.selected=text
+    this.originalText=text
 
   }
 
@@ -36,5 +38,21 @@ export class PrincipalComponent implements OnInit {
     var text= this.selected
     var index = this.appService.getList().indexOf(text)
     delete this.appService.getList()[index]
+  }
+
+  saveFavorite(){
+    this.appService.getList().push(this.originalText)
+  }
+
+  cypherText(){
+    var s = this.originalText;
+    var chr, i = 0, l = s.length, out = '';
+    for( ; i < l; i ++ ){
+        chr = s.charCodeAt( i ).toString( 2 );
+        while( chr.length % 8 != 0 ){ chr = '0' + chr; }
+        out += chr;
+    }
+    this.encrypText=out
+    
   }
 }
